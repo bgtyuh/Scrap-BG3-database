f609cda51785aa0db78eef6382f734f62aa22f28
bgtyuh <zaidimartin17@gmail.com>
2024-08-26 11:07:29 +0200

    Ajout de image_path à la bdd bg3_classes.db

--- classes.py
+++ classes.py
@@ -28,7 +28,7 @@ CREATE TABLE Classes (
     skill_proficiencies TEXT,
     spellcasting_ability TEXT,
     starting_equipment TEXT,
-    image_url TEXT
+    image_path TEXT
 )
 ''')
 
@@ -67,6 +67,7 @@ CREATE TABLE Subclasses (
     class_name TEXT,
     name TEXT,
     description TEXT,
+    image_path TEXT,
     FOREIGN KEY(class_name) REFERENCES Classes(name)
 )
 ''')
@@ -87,13 +88,13 @@ for class_data in data['classes']:
     c.execute('''
     INSERT INTO Classes (name, description, hit_points_at_level1, hit_points_on_level_up, key_abilities,
                          saving_throw_proficiencies, equipment_proficiencies, skill_proficiencies, spellcasting_ability,
-                         starting_equipment, image_url)
+                         starting_equipment, image_path)
     VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
     ''', (class_data['name'], class_data['description'], class_data['hit_points_at_level1'],
           class_data['hit_points_on_level_up'], class_data['key_abilities'], class_data['saving_throw_proficiencies'],
           class_data['equipment_proficiencies'], class_data['skill_proficiencies'],
           class_data.get('spellcasting_ability', None),
-          class_data['starting_equipment'], class_data.get('image_url', None)))
+          class_data['starting_equipment'], class_data.get('image_path', None)))
 
     class_name = class_data['name']
 
@@ -121,9 +122,9 @@ for class_data in data['classes']:
     # Insert subclass data
     for subclass in class_data.get('subclasses', []):
         c.execute('''
-        INSERT INTO Subclasses (class_name, name, description)
-        VALUES (?, ?, ?)
-        ''', (class_name, subclass['name'], subclass['description']))
+        INSERT INTO Subclasses (class_name, name, description, image_path)
+        VALUES (?, ?, ?, ?)
+        ''', (class_name, subclass['name'], subclass['description'], subclass['image_path']))
 
         subclass_name = subclass['name']
 
